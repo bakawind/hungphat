dae29b3ec20dc35531361bdfd539d8df8d88eaa7
Nguyen Phuc Khang <nguyenphuckhang@gmail.com>
2012-08-18 17:03:23 +0700

    change schema

--- protected/data/hungphat_schema.sql
+++ protected/data/hungphat_schema.sql
@@ -1,32 +1,34 @@
-create table categories{
+create table categories(
     id int primary key,
     name varchar(128)
-};
+);
 
-create table products {
+create table products (
     id int primary key,
     code varchar(64) unique not null,
     name varchar(128) not null,
     price double not null default 0,
-    description varchar(max),
-    image varchar(128),
+    description text,
+    image varchar(256),
     modified_date datetime,
-    category_id int foreign key references categories(id)
-}
+    category_id int,
+    foreign key (category_id) references categories(id)
+);
 
-create table product_photos {
+create table product_photos (
     id int primary key,
-    url varchar(128) not null,
-    product_id int foreign key references products(id)
-}
+    url varchar(256) not null,
+    product_id int,
+    foreign key (product_id) references products(id)
+);
 
-create table article {
+create table article (
     id int primary key,
     title varchar(128),
-    content varchar(max) not null
-}
+    content text not null
+);
 
-create table orders {
+create table orders (
     id int primary key,
     name varchar(128) not null,
     email varchar(128),
@@ -34,11 +36,13 @@ create table orders {
     phone varchar(16) not null,
     status int not null default 0,
     total double not null,
-}
+);
 
-create table order_items {
+create table order_items (
     id int primary key,
-    product_id int foreign key references products(id) not null,
-    quntity int not null,
-    order_id int foreign key references orders(id) not null
-}
+    quantity int not null,
+    product_id int not null,
+    foreign key (product_id) references products(id),
+    order_id int not null,
+    foreign key (order_id) references orders(id)
+);
