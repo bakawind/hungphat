cad26bef8bf445cce4470fbbf9abde0e606fed24
unknown <Huy Nam>
2012-09-13 04:05:47 +0700

    add yahoo status plugin

--- protected/views/site/about.php
+++ protected/views/site/about.php
@@ -1,1 +1,9 @@
-askjasljdfljsd
+
+<?php
+$id = 'happystorm_12'; 
+ $status = file_get_contents("http://opi.yahoo.com/online?u=$id&m=a&t=1");
+if ($status === '00')
+	echo 'off';
+elseif ($status === '01')
+	echo 'on';  
+?>
