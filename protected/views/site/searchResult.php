3e79a22d26d9ab41b64d1745301e498d3d454e64
Nguyen Phuc Khang <nguyenphuckhang@gmail.com>
2012-09-19 13:57:15 +0700

    fix some tweak on searchResult

--- protected/views/site/searchResult.php
+++ protected/views/site/searchResult.php
@@ -1,17 +1,21 @@
 ﻿<div class='inner_content solid_background'>
-<h1>Kết quả cho tin tức</h1>
 <?php
+if ($articleResult->getTotalItemCount() > 0) {
 	$this->widget('zii.widgets.CListView', array(
 	    'dataProvider'=>$articleResult,
 	    'itemView'=>'_single_result',
 	    'summaryText'=>'',
-)); ?>
+    ));
+}
+?>
 	<br class='clear' />
-<h1>Kết quả cho sản phẩm</h1>
 <?php
+if ($productResult->getTotalItemCount() > 0) {
 	$this->widget('zii.widgets.CListView', array(
 	    'dataProvider'=>$productResult,
 	    'itemView'=>'_single_result',
 	    'summaryText'=>'',
-    )); ?>
+    ));
+}
+?>
 </div> <!-- end inner content-->
