98c3bbbe29351acdc472c4443a66f1fc3333f802
unknown <Huy Nam>
2012-09-21 12:22:33 +0700

    fix searchText

--- protected/views/site/searchResult.php
+++ protected/views/site/searchResult.php
@@ -1,7 +1,9 @@
 <div class='inner_content solid_background'>
+
 <?php
-if ($articleResult->getTotalItemCount() > 0) {
-	$this->widget('zii.widgets.CListView', array(
+if ($articleResult->getTotalItemCount() > 0) {?>
+	<h1> Kết quả cho Tin Tức</h1>
+<?php $this->widget('zii.widgets.CListView', array(
 	    'dataProvider'=>$articleResult,
 	    'itemView'=>'_single_result',
 	    'summaryText'=>'',
@@ -9,8 +11,11 @@ if ($articleResult->getTotalItemCount() > 0) {
 }
 ?>
 	<br class='clear' />
+
 <?php
-if ($productResult->getTotalItemCount() > 0) {
+if ($productResult->getTotalItemCount() > 0) {?>
+	<h1> Kết quả cho Sản Phẩm</h1>
+<?php
 	$this->widget('zii.widgets.CListView', array(
 	    'dataProvider'=>$productResult,
 	    'itemView'=>'_single_result',
