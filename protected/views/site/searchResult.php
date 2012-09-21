e3e2ebe7dd40d0805adddb933bedc1975c06a7b7
Nguyen Phuc Khang <nguyenphuckhang@gmail.com>
2012-09-21 12:05:52 +0700

    show some texts when there is no searching result

--- protected/views/site/searchResult.php
+++ protected/views/site/searchResult.php
@@ -1,21 +1,27 @@
 ﻿<div class='inner_content solid_background'>
 <?php
-if ($articleResult->getTotalItemCount() > 0) {
-	$this->widget('zii.widgets.CListView', array(
-	    'dataProvider'=>$articleResult,
-	    'itemView'=>'_single_result',
-	    'summaryText'=>'',
-    ));
-}
+if ($articleResult->getTotalItemCount() == 0 && $productResult->getTotalItemCount() == 0) {
+?>
+    Không tìm thấy kết quả nào
+<?php
+} else {
+    if ($articleResult->getTotalItemCount() > 0) {
+	    $this->widget('zii.widgets.CListView', array(
+	        'dataProvider'=>$articleResult,
+	        'itemView'=>'_single_result',
+	        'summaryText'=>'',
+        ));
+    }
 ?>
 	<br class='clear' />
 <?php
-if ($productResult->getTotalItemCount() > 0) {
-	$this->widget('zii.widgets.CListView', array(
-	    'dataProvider'=>$productResult,
-	    'itemView'=>'_single_result',
-	    'summaryText'=>'',
-    ));
+    if ($productResult->getTotalItemCount() > 0) {
+	    $this->widget('zii.widgets.CListView', array(
+	        'dataProvider'=>$productResult,
+	        'itemView'=>'_single_result',
+	        'summaryText'=>'',
+        ));
+    }
 }
 ?>
 </div> <!-- end inner content-->
