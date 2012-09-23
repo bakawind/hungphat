a11cb39a8c00d042c9de8259da816839c004bd10
Nguyen Phuc Khang <nguyenphuckhang@gmail.com>
2012-09-24 00:53:34 +0700

    fix some tweaks

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -5,8 +5,10 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<h2>Error <?php echo $code; ?></h2>
+<div class='container solid_background'>
+    <h2>Error <?php echo $code; ?></h2>
 
-<div class="error">
-<?php //echo CHtml::encode($message); ?>
+    <div class="error">
+    <?php //echo CHtml::encode($message); ?>
+    </div>
 </div>
